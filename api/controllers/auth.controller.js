d54940db4d261a053d1c8007f158c048ed3942d0
anik-saha-2001 <aniksahamda@gmail.com>
2024-05-07 23:18:11 +0530

    complete signup controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,12 +3,19 @@ import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 
 export const signUpUser = async (req, res, next) => {
-  const { username, email, password } = req.body;
-  if (!username || !email || !password) {
+  const { username, email, password, accountType, phone, goal } = req.body;
+  if (!username || !email || !password || !accountType || !phone) {
     next(errorHandler(400, "All fields are required"));
   }
   const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashedPassword });
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+    phone,
+    goal,
+    accountType,
+  });
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { type } from "os";
 
 const userSchema = new mongoose.Schema(
   {
@@ -15,6 +16,20 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
+    },
+    phone: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    goal: {
+      type: String,
+      default: "",
+    },
+    accountType: {
+      type: String,
+      required: true,
+      enum: ["mentor", "mentee"],
     },
   },
   { timestamps: true }
