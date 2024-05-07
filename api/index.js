eb2f9a992aa8f0098fca49871730e6dde46d5ff8
anik-saha-2001 <aniksahamda@gmail.com>
2024-05-07 22:06:19 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
