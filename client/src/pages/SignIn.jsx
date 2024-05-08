882c9b014bb32953a70675eb3de7e5ef8c2f0624
anik-saha-2001 <aniksahamda@gmail.com>
2024-05-08 18:47:17 +0530

    complete sign in functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,76 +1,94 @@
-import { Link } from "react-router-dom";
-<<<<<<< HEAD
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-  return (
-    <div>
-      <div className="p-3 max-w-lg mx-auto">
-        <h1 className="text-3xl text-center font-semibold my-4">Sign In</h1>
-        <form className="flex flex-col gap-4">
-          <input
-            type="email"
-            placeholder="Email"
-            id="email"
-            className="bg-slate-100 p-3 rounded-lg"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            id="password"
-            className="bg-slate-100 p-3 rounded-lg"
-          />
-          <button
-            type="submit"
-            className="bg-slate-700 p-3 rounded-lg text-white uppercase hover:opacity-90 disabled:opacity-70"
-          >
-            Sign In
-          </button>
-        </form>
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-        <div className="flex gap-2 mt-5">
-          <p>New User?</p>
-          <Link to="/sign-up">
-            <span className="text-blue-800 hover:underline">Sign Up</span>
-          </Link>
-        </div>
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(null);
+
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      setLoading(false);
+
+      if (data.success === false) {
+        return setError(data.message);
+      }
+
+      if (res.ok) {
+        navigate("/home");
+      }
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-4">Sign In</h1>
+
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          required
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          required
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+
+        <button
+          disabled={loading}
+          type="submit"
+          className="bg-slate-700 p-3 rounded-lg text-white uppercase hover:opacity-90 disabled:opacity-70"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+
+      <div className="flex gap-2 mt-5">
+        <p>Don&apos;t have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-800 hover:underline">Sign Up</span>
+        </Link>
       </div>
+      {error && (
+        <div className="bg-red-300 p-3 rounded-lg mt-5 text-red-500">
+          {error}
+        </div>
+      )}
     </div>
   );
-=======
-import SignUp from "./SignUp";
-
-const SignIn = () => {
-  return <div><div className="p-3 max-w-lg mx-auto">
-  <h1 className="text-3xl text-center font-semibold my-4">Sign Up</h1>
-  <form className="flex flex-col gap-4">
-    <input
-      type="email"
-      placeholder="Email"
-      id="email"
-      className="bg-slate-100 p-3 rounded-lg"
-    />
-    <input
-      type="password"
-      placeholder="Password"
-      id="password"
-      className="bg-slate-100 p-3 rounded-lg"
-    />
-    <button
-      type="submit"
-      className="bg-slate-700 p-3 rounded-lg text-white uppercase hover:opacity-90 disabled:opacity-70"
-    >
-      Sign Inw
-    </button>
-  </form>
-
-  <div className="flex gap-2 mt-5">
-    <p>New User?</p>
-    <Link to="/sign-up">
-      <span className="text-blue-800 hover:underline">Sign Up</span>
-    </Link>
-  </div>
-</div></div>;
->>>>>>> 2845650d49e6eb95942ff5746008b613b1523830
 };
 
 export default SignIn;
