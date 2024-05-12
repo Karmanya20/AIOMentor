9b4ed4b5b96050c6c5c63042273372b09823f136
badal kumar singh <95132609+Badalsingh07@users.noreply.github.com>
2024-05-12 21:12:53 +0530

    Fixed lock file issue

--- api/controllers/profile.controller.js
+++ api/controllers/profile.controller.js
@@ -1,21 +1,24 @@
 const User = require("../models/user.model");
-const Profile = require("../models/profile.model");
+
 exports.updateProfile = async (req, res) => {
     try {
-      const { dateOfBirth = "", goal = "", contactNumber } = req.body;
+      const { dateOfBirth = "", goal = "", phone ="",location="",industry="",area_of_expertise=""} = req.body;
       const id = req.user.id;
+     
   
-      // Find the profile by id
-      const userDetails = await User.findById(id);
-      const profile = await Profile.findById(userDetails.additionalDetails);
+     
   
       // Update the profile fields
-      profile.dateOfBirth = dateOfBirth;
-      profile.about = about;
-      profile.contactNumber = contactNumber;
+      User.dateOfBirth = dateOfBirth;
+      User.goal = goal;
+      User.phone = phone;
+      User.location=location;
+      User.industry=industry;
+      User.area_of_expertise=area_of_expertise;
+
   
       // Save the updated profile
-      await profile.save();
+      await User.save();
   
       return res.json({
         success: true,
@@ -47,9 +50,8 @@ exports.updateProfile = async (req, res) => {
           message: "User not found",
         });
       }
-      // Delete Assosiated Profile with the User
-      await Profile.findByIdAndDelete({ _id: user.additionalDetails });
-      // TODO: Unenroll User From All the Enrolled Courses
+      
+      
       // Now Delete User
       await User.findByIdAndDelete({ _id: id });
       res.status(200).json({
@@ -61,6 +63,34 @@ exports.updateProfile = async (req, res) => {
       res
         .status(500)
         .json({ success: false, message: "User Cannot be deleted successfully" });
+    }
+  };
+  exports.updateDisplayPicture = async (req, res) => {
+    try {
+      const displayPicture = req.files.displayPicture;
+      const userId = req.user.id;
+      const image = await uploadImageToCloudinary(
+        displayPicture,
+        process.env.FOLDER_NAME,
+        1000,
+        1000
+      );
+      console.log(image);
+      const updatedProfile = await User.findByIdAndUpdate(
+        { _id: userId },
+        { image: image.secure_url },
+        { new: true }
+      );
+      res.send({
+        success: true,
+        message: `Image Updated successfully`,
+        data: updatedProfile,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      });
     }
   };
 
